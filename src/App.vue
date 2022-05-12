f44625db130fbf56cbe10d574e0e241746d5dfe7
Ciuff4 <gasperini_cristina@libero.it>
2022-05-12 18:10:17 +0200

    push immagine

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
     <TitleCom/>
+    <HeroComp/>
   </div>
 </template>
 
 <script>
 import TitleCom from './components/TitleCom.vue'
+import HeroComp from './components/HeroComp.vue'
 
 export default {
   name: 'App',
   components: {
-    TitleCom
+    TitleCom,
+    HeroComp
   }
 }
 </script>
---
+++ src/components/HeroComp.vue
@@ -0,0 +1,14 @@
+<template>
+    <immagine>
+        <img src="../assets/logo.png" alt="">
+    </immagine>
+</template>
+
+<script>
+export default {
+name:'HeroComp'
+}
+</script>
+
+<style>
+</style>
